2ed238e38efcfffcc3cb42df6adabbba17f3e175
sansoft007 <santosh.sansoft@gmail.com>
2022-06-29 23:46:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>E-Come Project</h1>\
+    <button>Normal button</button>
+    <Button>Bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
